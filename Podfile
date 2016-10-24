d02d25340bc4269fa1fa648e2f83a634b132e8e8
Robin Oster <robin.oster@rascor.com>
2016-10-24 13:22:54 +0200

    Cleanup old Pods data and add Source folder.

--- Podfile
+++
@@ -1,7 +0,0 @@
-platform :ios, '9.0'
-
-target 'ROGoogleTranslate' do
-  use_frameworks!
-
-  pod 'SwiftyJSON'
-end
--- Pods/Target Support Files/Pods-ROGoogleTranslate/Pods-ROGoogleTranslate-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ROGoogleTranslate : NSObject
-@end
-@implementation PodsDummy_Pods_ROGoogleTranslate
-@end
